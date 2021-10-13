ed5f205537af78f4f8b984697b4f76cb88d023ad
jfran <jfranciscopages@gmail.com>
2021-10-13 02:20:37 -0300

    Login con logo, alertas y spinner

--- front/hooks/useCreateUser.js
+++ front/hooks/useCreateUser.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import expoLocalHost from "../localHost";
-import { useToast } from "native-base";
-import { Box } from "native-base";
+import { useToast, Box } from "native-base";
 import { useNavigation } from "@react-navigation/core";
 
 const useCreateUser = () => {
